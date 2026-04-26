2180503e8b294af62103f17d5e07c14e34b3a73b
Rupesh Ravsaheb Chavan <152914283+rupesh2825@users.noreply.github.com>
2026-04-26 17:21:26 +0530

    index.js

--- Backend/index.js
+++ Backend/index.js
@@ -28,9 +28,10 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
 app.get('/', async (req, res) => {
   try {
     const result = await Stud.find();
